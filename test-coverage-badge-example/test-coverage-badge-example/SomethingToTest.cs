993d51c2f98eedd333597acf9abd73b0bc8644ab
Christian Fredh <christian.fredh@gmail.com>
2023-09-11 17:34:37 +0200

    test example

---
+++ test-coverage-badge-example/test-coverage-badge-example/SomethingToTest.cs
@@ -0,0 +1,15 @@
+﻿namespace test_coverage_badge_example;
+
+public class SomethingToTest
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a + b;
+    }
+}
+
---
+++ test-coverage-badge-example/tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using test_coverage_badge_example;
+
+namespace tests;
+
+public class Tests
+{
+    [Test]
+    public void Add_Test()
+    {
+        var sut = new SomethingToTest();
+
+        var result = sut.Add(5, 3);
+        Assert.That(result, Is.EqualTo(8));
+    }
+
+    [Test]
+    public void Multiply_Test()
+    {
+        var sut = new SomethingToTest();
+
+        var result = sut.Multiply(5, 3);
+        Assert.That(result, Is.EqualTo(15));
+    }
+}
